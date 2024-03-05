7c86df11120cf3a7fa77a471a3a0e963983ab942
songyunchao <songyunchao@wufangzhai.com>
2024-03-05 14:45:07 +0800

    创建一个flutter工程

---
+++ android/app/src/main/kotlin/com/syc/study_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.syc.study_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/first_flutter_main.dart
@@ -0,0 +1,56 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return   ChangeNotifierProvider(
+      create: ( context)=>MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange)
+        ),
+        home: const MyHomePage(),
+      ),   
+    );
+  }
+}
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  void getNext(){
+    current =WordPair.random();
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('a random idea:'),
+          Text(appState.current.asLowerCase),
+          ElevatedButton(
+            onPressed: () {
+              appState.getNext();
+            }, 
+            child: const Text('Next'),
+          )
+        ],
+      ),
+    );
+  }
+}
+
